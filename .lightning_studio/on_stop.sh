973da8ea51a00eebd122fc6d58789a73c559ab9f
challenger <68114783+Motsepe-Jr@users.noreply.github.com>
2024-08-29 20:52:43 +0200

    Delete .lightning_studio directory

--- .lightning_studio/on_stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script runs every time your Studio sleeps, from your home directory.
-
-# Add your shutdown commands below.
-#
-# Example: docker down my-container
-# Example: sudo service mysql stop
